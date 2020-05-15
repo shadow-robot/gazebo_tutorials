73ecade1970f7db663fb65bd0a5d50c7edc4142f
Toni Oliver <toni@shadowrobot.com>
2020-05-15 08:33:54 +0000

    Test SetParam fmax and vel with gravity

--- set_velocity/examples/set_vel_plugin/src/SetJointVelocityPlugin.cpp
+++ set_velocity/examples/set_vel_plugin/src/SetJointVelocityPlugin.cpp
@@ -40,31 +40,21 @@ namespace gazebo
       {
         if (update_num == 0)
         {
-          // Joint velocity instantaneously without applying forces
-          this->model->GetJoint("gray_joint")->SetVelocity(0, 1.0);
+          this->model->GetJoint("world_joint")->SetParam("fmax", 0, 10000.0);
+          this->model->GetJoint("world_joint")->SetParam("vel", 0, 0.0);
+          this->model->GetJoint("blue_joint")->SetParam("fmax", 0, 2500.0);
+          this->model->GetJoint("blue_joint")->SetParam("vel", 0, 0.0);
 
-          // Joint velocity using joint motors
-          this->model->GetJoint("orange_joint")->SetParam("fmax", 0, 100.0);
-          this->model->GetJoint("orange_joint")->SetParam("vel", 0, 1.0);
+          this->model->GetJoint("ra_shoulder_pan_joint")->SetParam("fmax", 0, 10000.0);
+          this->model->GetJoint("ra_shoulder_pan_joint")->SetParam("vel", 0, 0.0);
+          this->model->GetJoint("ra_shoulder_lift_joint")->SetParam("fmax", 0, 2500.0);
+          this->model->GetJoint("ra_shoulder_lift_joint")->SetParam("vel", 0, 0.0);
+        }
+        this->model->GetJoint("world_joint")->SetParam("vel", 0, 0.0);
+        this->model->GetJoint("blue_joint")->SetParam("vel", 0, 0.0);
 
-          // Joint velocity using PID controller
-          this->jointController.reset(new physics::JointController(
-                this->model));
-          this->jointController->AddJoint(model->GetJoint("purple_joint"));
-          std::string name = model->GetJoint("purple_joint")->GetScopedName();
-          this->jointController->SetVelocityPID(name, common::PID(100, 0, 0));
-          this->jointController->SetVelocityTarget(name, 1.0);
-        }
-        else if (update_num < 200)
-        {
-          // Must update PID controllers so they apply forces
-          this->jointController->Update();
-        }
-        else if (update_num == 200)
-        {
-          // Joint motors are disabled by setting max force to zero
-          this->model->GetJoint("orange_joint")->SetParam("fmax", 0, 0.0);
-        }
+        this->model->GetJoint("ra_shoulder_pan_joint")->SetParam("vel", 0, 0.0);
+        this->model->GetJoint("ra_shoulder_lift_joint")->SetParam("vel", 0, 0.0);
         update_num++;
       }
 
